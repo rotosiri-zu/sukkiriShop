ca5e923d518251a430e76210dc2325d0e4e50002
ひろき <@DESKTOP-PAA8BNH>
2024-09-10 17:56:52 +0900

    LoginLogicをテストするクラス

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -8,11 +8,11 @@ public class Account {
   private int age;
   
   public Account(String userId, String pass, String mail, String name, int age) {
-	  this.userId = userId;
-	  this.pass = pass;
-	  this.mail = mail;
-	  this.name = name;
-	  this.age = age;
+    this.userId = userId;
+	this.pass = pass;
+	this.mail = mail;
+	this.name = name;
+	this.age = age;
   }
   public String getUserId() { return userId; }
   public String getPass() { return pass; }
